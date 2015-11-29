626648d9d07166774c624f7b68dd157427ceeb45
Ian Malcolm <ian.h.0628@gmail.com>
2015-11-29 21:22:12 +0800

    Create maven project

---
+++ src/main/java/net/ianbox/www/LowCostSLAM/App.java
@@ -0,0 +1,13 @@
+package net.ianbox.www.LowCostSLAM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
